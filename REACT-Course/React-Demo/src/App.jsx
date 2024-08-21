23220cd3f1d0b43c69a643a11e15dbeb51fb0ea8
Anatoli Pavlov <18app81@gmail.com>
2024-08-21 14:55:21 +0300

    Create a Header element

--- REACT-Course/React-Demo/src/App.jsx
+++ REACT-Course/React-Demo/src/App.jsx
@@ -1,10 +1,17 @@
+import Header from './components/Header';
+import Body from './components/Body';
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      
+
+      <Header />
+
+      <Body />
+
+
     </div>
   )
 }
