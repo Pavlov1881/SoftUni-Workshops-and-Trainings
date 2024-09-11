db9eaa25d73ff348cb11b6c2ecea0dce05825cb3
Anatoli Pavlov <18app81@gmail.com>
2024-09-11 13:39:29 +0300

    Add Header component

--- REACT-Course/Components-Workshop/client/src/App.jsx
+++ REACT-Course/Components-Workshop/client/src/App.jsx
@@ -1,11 +1,15 @@
 
+import Header from './components/Header';
 import './styles.css';
 
 function App() {
 
     return (
         <div>
-            <h1>It works :)</h1>
+
+            <Header />
+
+
         </div>
     );
 }
